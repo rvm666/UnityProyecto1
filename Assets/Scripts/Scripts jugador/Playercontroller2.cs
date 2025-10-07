f5385ee08c080e035e7625608c81be2aaa5cebd7
rvm666 <rvm666@educa.madrid.org>
2025-10-07 23:36:59 +0200

    Terminado

--- Assets/Scripts/Scripts jugador/Playercontroller2.cs
+++ Assets/Scripts/Scripts jugador/Playercontroller2.cs
@@ -4,21 +4,34 @@ using UnityEngine.InputSystem;
 public class Playercontroller2 : MonoBehaviour
 {
     private Vector2 movementInput;
+    Rigidbody2D rb;
     public float speed = 1f;
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
     
     public void OnMove(InputValue value)
     {
         movementInput = value.Get<Vector2>();
+        //movementInput.x = Input.GetAxisRaw("Horizontal");
+        //movementInput.y = Input.GetAxisRaw("Vertical");
+        //movementInput = value.Get<Vector2>();
     }
 
-    void Update()
+    void FixedUpdate()
     {
-        Vector3 movement = new Vector3(movementInput.x, movementInput.y, 0f);
-
-        transform.Translate(movement * speed * Time.deltaTime, Space.World);
+        rb.linearVelocity = movementInput.normalized * speed;
+        //Vector3 movement = new Vector3(movementInput.x, movementInput.y, 0f);
+        //transform.Translate(movement * speed * Time.deltaTime, Space.World);
     }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            Debug.Log("¡Muerte!");
+        }
+            
+    }
+
 }
